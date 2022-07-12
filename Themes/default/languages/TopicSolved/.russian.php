e25c24bec9be41dce9235c644093920264a155bf
Bugo <bugo@dragomano.ru>
2022-07-12 08:09:50 +0500

    Add Russian translation

---
+++ Themes/default/languages/TopicSolved/.russian.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @package Topic Solved
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ */
+
+// Main
+$txt['TopicSolved_settings'] = 'Настройки мода Topic Solved';
+$txt['TopicSolved_mark_solved'] = 'Отметить как решенную';
+$txt['TopicSolved_mark_unsolved'] = 'Отметить как нерешенную';
+
+// Boards
+$txt['TopicSolved_board_solve'] = 'Можно отмечать темы как решенные';
+$txt['TopicSolved_boards_select'] = 'Выберите разделы, в которых можно отмечать темы как решенные';
+
+// Error
+$txt['TopicSolved_error_no_topic'] = 'Тема не указана';
+$txt['TopicSolved_error_no_board'] = 'Этот раздел не настроен для работы с решенными темами';
+$txt['cannot_solve_topics_own'] = 'Вы не можете помечать собственные темы как решенные';
+$txt['cannot_solve_topics_any'] = 'Вы не можете помечать темы как решенные';
+
+// Permissions
+$txt['permissionname_solve_topics'] = 'Пометка тем как решенных';
+$txt['permissionname_solve_topics_own'] = 'Возможность отмечать собственные темы как решенные';
+$txt['permissionname_solve_topics_any'] = 'Возможность отмечать любые темы как решенные';
+$txt['permissionhelp_solve_topics'] = 'Это разрешение позволяет пользователям отмечать темы как решенные';
