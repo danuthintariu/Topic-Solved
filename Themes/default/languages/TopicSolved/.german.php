421d9bf50fba51f7bef01bac8cd1834b7f0370bd
Diego Andrés <diegoandres_cortes@outlook.com>
2024-09-02 23:24:50 -0600

    feat: add german translation

---
+++ Themes/default/languages/TopicSolved/.german.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @package Topic Solved
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ */
+
+// Administratoren-Bereich
+$txt['TopicSolved_settings'] = 'Einstellungen für gelöstes Thema';
+$txt['TopicSolved_mark_solved'] = 'Als gelöst markieren';
+$txt['TopicSolved_mark_unsolved'] = 'Als ungelöst markieren';
+
+// Boards
+$txt['TopicSolved_board_solve'] = 'Kann Themen als gelöst markieren';
+$txt['TopicSolved_boards_select'] = 'Boards auswählen, in denen Sie Themen als gelöst markieren können';
+
+// Fehler
+$txt['TopicSolved_error_no_topic'] = 'Kein Thema angegeben';
+$txt['TopicSolved_error_no_board'] = 'Dieses Forum ist nicht für die Lösung von Themen konfiguriert';
+$txt['cannot_solve_topics_own'] = 'Sie können Ihre eigenen Themen nicht lösen';
+$txt['cannot_solve_topics_any'] = 'Sie können in diesem Forum keine Themen lösen';
+
+// Berechtigungen
+$txt['permissionname_solve_topics'] = 'Themen als gelöst markieren';
+$txt['permissionname_solve_topics_own'] = 'Eigene Themen als gelöst markieren';
+$txt['permissionname_solve_topics_any'] = 'Jedes Thema als gelöst markieren';
+$txt['permissionhelp_solve_topics'] = 'Diese Berechtigung ermöglicht es einem Benutzer, ein Thema als gelöst zu markieren';
