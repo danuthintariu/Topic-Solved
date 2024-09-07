0937549b6eecee3217267e10f870f6d9af517c3e
Diego Andrés <diegoandres_cortes@outlook.com>
2024-09-06 20:21:36 -0600

    feat: add portuguese (pt) translation

---
+++ Themes/default/languages/TopicSolved/.portuguese_pt.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @package Topic Solved
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2022, SMF Tricks
+ */
+
+// Main
+$txt['TopicSolved_settings'] = 'Configurações do tópico resolvido';
+$txt['TopicSolved_mark_solved'] = 'Marcar como resolvido';
+$txt['TopicSolved_mark_unsolved'] = 'Marcar como não resolvido';
+
+// Boards
+$txt['TopicSolved_board_solve'] = 'Can mark topics as solved';
+$txt['TopicSolved_boards_select'] = 'Selecione painéis onde você pode marcar tópicos como resolvidos';
+
+// Error
+$txt['TopicSolved_error_no_topic'] = 'Nenhum tópico especificado';
+$txt['TopicSolved_error_no_board'] = 'Este quadro não está configurado para permitir a resolução de tópicos';
+$txt['cannot_solve_topics_own'] = 'Você não pode resolver seus próprios tópicos';
+$txt['cannot_solve_topics_any'] = 'Você não pode resolver tópicos neste quadro';
+
+// Permissions
+$txt['permissionname_solve_topics'] = 'Marcar tópicos como resolvidos';
+$txt['permissionname_solve_topics_own'] = 'Marcar seus próprios tópicos como resolvidos';
+$txt['permissionname_solve_topics_any'] = 'Marcar qualquer tópico como resolvido';
+$txt['permissionhelp_solve_topics'] = 'Esta permissão permite que um usuário marque um tópico como resolvido';
