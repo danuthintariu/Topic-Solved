3fa63128d1dc83dfce92da1fc125d9bf8cc432e4
dennisb1 <dennisb1@gmail.com>
2025-02-14 10:02:06 +0100

    Update .dutch.php

--- Themes/default/languages/TopicSolved/.dutch.php
+++ Themes/default/languages/TopicSolved/.dutch.php
@@ -7,49 +7,49 @@
  * @copyright Copyright (c) 2022, SMF Tricks
  */
 
-// Main
-$txt['TopicSolved_settings'] = 'Topic Solved Instellingen';
-$txt['TopicSolved_mark_solved'] = 'Opgelost';
-$txt['TopicSolved_mark_unsolved'] = 'Niet opgelost';
+// Hoofdinstellingen
+$txt['TopicSolved_settings'] = 'Instellingen voor Opgeloste Topics';
+$txt['TopicSolved_mark_solved'] = 'Markeer als Opgelost';
+$txt['TopicSolved_mark_unsolved'] = 'Markeer als Niet Opgelost';
 
-// Boards
-$txt['TopicSolved_board_solve'] = 'Kan topic markeren als opgelost';
-$txt['TopicSolved_boards_select'] = 'Selecteer board waar je topics kan markeren als opgelost';
+// Forums
+$txt['TopicSolved_board_solve'] = 'Kan topics als opgelost markeren';
+$txt['TopicSolved_boards_select'] = 'Selecteer boards waarin je topics als opgelost kunt markeren';
 
-// Error
+// Foutmeldingen
 $txt['TopicSolved_error_no_topic'] = 'Geen topic gespecificeerd';
-$txt['TopicSolved_error_no_board'] = 'This board is not configured to allow solving topics';
-$txt['cannot_solve_topics_own'] = 'You cannot solve your own topics';
-$txt['cannot_solve_topics_any'] = 'You cannot solve topics in this board';
+$txt['TopicSolved_error_no_board'] = 'Dit board is niet geconfigureerd om topics als opgelost te markeren';
+$txt['cannot_solve_topics_own'] = 'Je kunt je eigen topics niet oplossen';
+$txt['cannot_solve_topics_any'] = 'Je kunt topics in dit board niet oplossen';
 
-// Permissions
-$txt['permissionname_solve_topics'] = 'Markeer topic als opgelost';
-$txt['permissionname_solve_topics_own'] = 'Mark their own topics as solved';
-$txt['permissionname_solve_topics_any'] = 'Mark any topic as solved';
-$txt['permissionhelp_solve_topics'] = 'This permission allows a user to mark a topic as solved';
+// Rechten
+$txt['permissionname_solve_topics'] = 'Topics als opgelost markeren';
+$txt['permissionname_solve_topics_own'] = 'Eigen topics als opgelost markeren';
+$txt['permissionname_solve_topics_any'] = 'Elk topic als opgelost markeren';
+$txt['permissionhelp_solve_topics'] = 'Deze toestemming staat een gebruiker toe om een topic als opgelost te markeren';
 
-// Settings
-$txt['TopicSolved_automove_enable'] = 'Enable auto moving solved or not solved topics to specfic boards';
-$txt['TopicSolved_automove_enable_desc'] = 'You can select the boards for each board when editing or adding boards within the forum admin. The topic is only moved when there is a change in the solved status.';
-$txt['TopicSolved_solve_automove'] = 'Status to auto move solved topics?';
-$txt['TopicSolved_solved_destination'] = 'Select destination board for auto moving solved topics';
-$txt['TopicSolved_notsolved_destination'] = 'Not solved destination';
-$txt['TopicSolved_indicatorclass_disable'] = 'Disable color and icon indicators for solved topics';
-$txt['TopicSolved_single_status'] = 'Use single status for auto-move';
-$txt['TopicSolved_single_status_desc'] = 'When a board has auto-move enabled, enable this setting to only ever show the status that causes topics to move. For example: "Not Solved" will not be displayed if the board moves topics with "Solved" status';
-$txt['TopicSolved_auto_solved'] = 'Solved';
-$txt['TopicSolved_auto_notsolved'] = 'Not Solved';
-$txt['TopicSolved_maint_board_solve'] = 'Solve Topics in a board';
-$txt['TopicSolved_maint_board_solve_desc'] = 'This action is unaffected by the auto-move setting, no topics will be moved when performing this action.';
-$txt['TopicSolved_maint_solve_status'] = 'Status to set for the topics';
-$txt['TopicSolved_maint_board_solve_save'] = 'Update now';
-$txt['TopicSolved_maint_board_solve_sure'] = 'Are you sure you want to change the status for all of the topics in the specificed board?';
+// Instellingen
+$txt['TopicSolved_automove_enable'] = 'Automatisch verplaatsen van opgeloste of niet-opgeloste topics inschakelen';
+$txt['TopicSolved_automove_enable_desc'] = 'Je kunt de boards selecteren voor elke sectie bij het bewerken of toevoegen van boards binnen de boardbeheerder. Het topic wordt alleen verplaatst als de oplossingsstatus verandert.';
+$txt['TopicSolved_solve_automove'] = 'Status om automatisch opgeloste topics te verplaatsen?';
+$txt['TopicSolved_solved_destination'] = 'Selecteer bestemmingsboard voor automatisch verplaatsen van opgeloste topics';
+$txt['TopicSolved_notsolved_destination'] = 'Niet opgeloste bestemming';
+$txt['TopicSolved_indicatorclass_disable'] = 'Kleur- en pictogramindicatoren voor opgeloste topics uitschakelen';
+$txt['TopicSolved_single_status'] = 'Enkele status voor automatisch verplaatsen gebruiken';
+$txt['TopicSolved_single_status_desc'] = 'Wanneer een board automatische verplaatsing heeft ingeschakeld, schakel deze instelling in om alleen de status weer te geven die topics verplaatst. Bijvoorbeeld: "Niet Opgelost" wordt niet weergegeven als het board topics verplaatst met de status "Opgelost"';
+$txt['TopicSolved_auto_solved'] = 'Opgelost';
+$txt['TopicSolved_auto_notsolved'] = 'Niet Opgelost';
+$txt['TopicSolved_maint_board_solve'] = 'Topics in een board oplossen';
+$txt['TopicSolved_maint_board_solve_desc'] = 'Deze actie wordt niet beïnvloed door de automatische verplaatsingsinstelling, geen topics worden verplaatst bij het uitvoeren van deze actie.';
+$txt['TopicSolved_maint_solve_status'] = 'Status instellen voor de topics';
+$txt['TopicSolved_maint_board_solve_save'] = 'Nu bijwerken';
+$txt['TopicSolved_maint_board_solve_sure'] = 'Weet je zeker dat je de status voor alle topics in het opgegeven board wilt wijzigen?';
 
-// Loggging
-$txt['TopicSolved_log'] = 'Solved Topics Log';
-$txt['TopicSolved_log_desc'] = 'Use this area to view the topics marked as solved.';
-$txt['TopicSolved_no_logs'] = 'No topics have been solved.';
-$txt['TopicSolved_log_marked_solved'] = '<a href="%1$s">%2$s</a> marked as solved.';
-$txt['TopicSolved_log_marked_not_solved'] = '<a href="%1$s">%2$s</a> marked as not solved.';
-$txt['TopicSolved_moved_log'] = 'Destination Board';
-$txt['modlog_ac_clearlog_solved'] = 'Cleared the solved topics log';
+// Logboek
+$txt['TopicSolved_log'] = 'Logboek opgeloste topics';
+$txt['TopicSolved_log_desc'] = 'Gebruik dit gebied om de als opgelost gemarkeerde topics te bekijken.';
+$txt['TopicSolved_no_logs'] = 'Er zijn geen topics opgelost.';
+$txt['TopicSolved_log_marked_solved'] = '<a href="%1$s">%2$s</a> gemarkeerd als opgelost.';
+$txt['TopicSolved_log_marked_not_solved'] = '<a href="%1$s">%2$s</a> gemarkeerd als niet opgelost.';
+$txt['TopicSolved_moved_log'] = 'Bestemmingsboard';
+$txt['modlog_ac_clearlog_solved'] = 'Het logboek van opgeloste topics gewist';
